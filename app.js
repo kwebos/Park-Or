ee81ca651aa08c57de54d13cafcf6f566cec0c4b
Amir Gilboa <kwebos@gmail.com>
2015-04-09 01:05:50 +0300

    server login + coords

---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * Module dependencies.
+ */
+var express  = require('express');
+var connect  = require('connect');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var cookieParser = require('cookie-parser');
+// Configuration
+app.use(express.static(__dirname + '/public'));
+app.use(connect.logger('dev'));
+app.use(cookieParser());
+app.use(connect.json());
+app.use(connect.urlencoded());
+
+
+// load the cookie parsing middleware
+
+// Routes
+require('./routes/routes.js')(app);
+app.listen(port);
+console.log('The App runs on port ' + port);
