63021ae72798bc35aa8e2d92020f5d2a67c55d7c
Amir Gilboa <kwebos@gmail.com>
2015-08-18 21:17:58 +0300

    checking again

--- routes/routes.js
+++ routes/routes.js
@@ -53,8 +53,8 @@ module.exports = function(app) {
 	app.post('/location',function(req,res){
 		var longitude = req.body.longitude;
 		var latitude = req.body.latitude;
-		//var token = req.body.token;
-		//var user = new users({token: token});
+		var token = req.body.token;
+		var user = new users({token: token});
 		//console.log(user);
 		//console.log(token);		
 		var newparking = new parking({
